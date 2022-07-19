163fc5da516184d2f9517b79aab9a9fe235aac5e
Borsuk Vladimir <grifon26@inbox.ru>
2022-07-19 21:24:17 +0300

    Share in app does not works correctly.

--- app/src/main/java/ru/netology/nmedia/ui/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/MainActivity.kt
@@ -3,14 +3,13 @@ package ru.netology.nmedia.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.fragment.app.commit
+import androidx.navigation.navArgs
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity(){
-
-    //private val args by navArgs<PostContentFragmentArgs>()
-
+    private val args by navArgs<PostContentFragmentArgs>()
 override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val binding = MainActivityBinding.inflate(layoutInflater)
@@ -19,8 +18,21 @@ override fun onCreate(savedInstanceState: Bundle?) {
     if (intent.action != Intent.ACTION_SEND) return
     val text = intent.getStringExtra(Intent.EXTRA_TEXT)
     if (text.isNullOrBlank()) return
-    //args.initialContent
-    val action = FeedFragmentDirections.toPostContentFragment(text)
-    findNavController(R.id.nav_host_fragment).navigate(action)
+    //args.initialContent.apply { PostContentFragmentArgs(text) }
+    //println(args.initialContent)
+//    findNavController(R.id.nav_host_fragment).navigate(
+//        R.id.toPostContentFragment,
+//        PostContentFragmentArgs(text).toBundle()
+//    )
+//    args.initialContent { PostContentFragmentArgs(text).toBundle()}
+    //PostContentFragmentArgs(text).toBundle()
+    supportFragmentManager.commit {
+        val fragment = PostContentFragment()
+        fragment.arguments = PostContentFragmentArgs(text).toBundle()
+        add(R.id.nav_host_fragment, FeedFragment())
+        addToBackStack(null)
+        replace(R.id.nav_host_fragment, fragment)
+        addToBackStack(null)
+    }
 }
 }
